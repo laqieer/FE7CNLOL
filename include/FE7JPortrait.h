f891bfe9ece9d1d1e85bd729b8fd709badc3cdff
laqieer <laqieer@126.com>
2019-04-06 15:40:01 +0800

    define new portrait extension format. 新头像扩展格式定义.

--- include/FE7JPortrait.h
+++ include/FE7JPortrait.h
@@ -41,6 +41,16 @@ struct EyeFrameInfo{
 	s16 *blinkTemplate;	// 指向眨眼帧模板的指针(只对blinkOrWink2有效)
 };
 
+// 定义嘴部帧的信息
+struct MouthFrameInfo{
+	u16 width;	// 宽(单位:tile)
+	u16 height;	// 高(单位:tile)
+	u16 tileX; // 嘴部帧左上角在头像长条中的横坐标(单位:tile)
+	u16 tileY; // 嘴部帧左上角在头像长条中的纵坐标(单位:tile)
+	void *mouthFrame;	// 指向嘴部帧的指针(原来的嘴部帧指针)
+	s16 *mouthTemplate;	// 指向眨眼帧模板的指针(暂时不用,放在这里用于以后扩展)
+};
+
 // 扩展的头像指针结构体
 struct Portrait{
 	void *mainPortrait;	// 指向大头像的指针
@@ -70,6 +80,81 @@ struct Portrait{
 	s8 portraitMaskInBox:7;	// 人物数据界面头像框蒙版序号
 	s8 eyeMouthPositionAlignmentFlag:1;		// 最高位标识眼睛和嘴的坐标是否是8像素的倍数还是精细到像素
 };
+
+// 活动块组成的头像(会话、使用物品、转职等)
+typedef struct PortraitObject
+{
+	u8 width;	// width (unit: pixel). 宽 (单位: 像素).
+	u8 height;	// height (unit: pixel). 高 (单位: 像素).
+	s8 offsetX;	// x offset (unit: pixel). 横向偏移 (单位: 像素).
+	s8 offsetY;	// y offset (unit: pixel). 纵向偏移 (单位: 像素).
+	u16 *oamL; // Face to left. 头像朝左时的OAM.
+	u16 *oamR; // Face to Rifgt. 头像朝右时的OAM.
+}PortraitOBJ,*pPortraitOBJ;
+
+// 背景头像(人物能力界面等)
+typedef struct PortraitBackground
+{
+	u16 *tsa; // BG Map
+	u8 *mask; // 蒙版
+}PortraitBG,*pPortraitBG;
+
+// 眼睛动画
+typedef struct EyeAnimationInfo
+{
+	u8 width;	// width (unit: pixel). 宽 (单位: 像素).
+	u8 height;	// height (unit: pixel). 高 (单位: 像素).
+	u8 widthRight;	// right eye width (unit: pixel). 右眼的宽 (单位: 像素).
+	u8 heightBottom;	// bottom eye (monster?) height (unit: pixel). 下眼(怪兽?)的高 (单位: 像素).
+	int tileNo;	// top left tile number in the tileset. 左上角的tile编号.
+	u8 *frame;	// animation frame (3 frames). 动画帧图 (3帧). 
+				// Frame 0: open. 睁眼.
+				// Frame 1: half open half closed. 半睁半闭.
+				// Frame 2: closed. 闭眼.
+}EyeAnimation,*pEyeAnimation;
+
+// 嘴部动画
+typedef struct MouthAnimationInfo
+{
+	u8 width;	// width (unit: pixel). 宽 (单位: 像素).
+	u8 height;	// height (unit: pixel). 高 (单位: 像素).
+	s16 tileNo;	// top left tile number in the tileset. 左上角的tile编号.
+	u8 *frame;	// animation frame (6 frames). 动画帧图 (6帧). 
+				// Frame 0: open (smile). 张嘴 (微笑).
+				// Frame 1: half open half closed (smile). 半张半闭 (微笑).
+				// Frame 2: closed (smile). 闭嘴 (微笑).
+				// Frame 3: open. 张嘴.
+				// Frame 4: half open half closed. 半张半闭.
+				// Frame 5: closed. 闭嘴.
+}MouthAnimation,*pMouthAnimation;
+
+// 附加头像信息定义
+typedef struct PortraitInfoExtra
+{
+	pPortraitOBJ obj;	// dialogue portrait. 对话头像.
+	pPortraitBG bg;	// status screen portrait. 能力界面头像.
+	pEyeAnimation eye;	// eye animation. 眼睛动画
+	pMouthAnimation mouth;	// mouth animation. 嘴部动画.
+	char *name;	// portrait name. 头像名字.
+}PortraitExtra,*pPortraitExtra;
+
+// 新扩展头像信息结构体
+// 和原版以及旧扩展格式不兼容,用newFlag区分
+typedef struct PortraitInfoNew
+{
+	u8 *tileset; // 8x8 tile, 256x32 image. 长条图片.
+	u8 *mini; // mini portrait. 小头像.
+	u16 *palette; // palette. 调色板.
+	pPortraitExtra extra; // extra portrait info. 附加头像信息.
+	u8 *card; // class card. 职业卡牌.
+	s8 mouthX; // useless. 废弃.
+	s8 mouthY; // useless. 废弃.
+	s8 eyeX; // useless. 废弃.
+	s8 eyeY; // useless. 废弃.
+	u8 eyeFlag; // eye control flag. 1-Normal. 6-always closed.
+	s8 newFlag; // new portrait struct format flag (should be set to -1). 新头像信息格式标识, -1(0xFF).
+	PortraitID id; // self id. 头像序号.
+}PortraitNew,*pPortraitNew;
 
 // 大头像精灵组合模板
 
