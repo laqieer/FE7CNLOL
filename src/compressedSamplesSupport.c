dc606b5c27f9569c0fc482b8ad0ed409700193dc
laqieer <laqieer@126.com>
2021-10-05 01:20:25 +0800

    压缩音源解码用表

--- src/compressedSamplesSupport.c
+++ src/compressedSamplesSupport.c
@@ -13,3 +13,24 @@ const int soundMainRAMFunc = SoundMainRAM_Buffer | 1;
 
 __attribute__((section(".soundMainRAMWordCnt")))
 const short soundMainRAMWordCnt = 0x200;
+
+// This is a table of deltas between sample values in compressed PCM data.
+const signed char gDeltaEncodingTable[] =
+{
+      0,
+      1,
+      4,
+      9,
+     16,
+     25,
+     36,
+     49,
+    -64,
+    -49,
+    -36,
+    -25,
+    -16,
+     -9,
+     -4,
+     -1,
+};
