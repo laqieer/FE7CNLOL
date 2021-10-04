fb4399d773dc485109c2ce40774718f31ffdb20c
laqieer <laqieer@126.com>
2021-10-05 01:06:59 +0800

    重新分配IWRAM空间给移植过来的m4a引擎,从0x400字节扩展到0x800字节

--- src/compressedSamplesSupport.c
+++ src/compressedSamplesSupport.c
@@ -1,4 +1,15 @@
+//#define SoundMainRAM_Buffer 0x3002378
+#define SoundMainRAM_Buffer 0x3005DA0
 void SoundMainRAM();
 
-__attribute__((section(".compressedSamplesSupport")))
-const int compressedSamplesSupport = &SoundMainRAM;
+__attribute__((section(".soundMainRAMSrc")))
+const int soundMainRAMSrc = &SoundMainRAM;
+
+__attribute__((section(".soundMainRAMBuffer")))
+const int soundMainRAMBuffer = SoundMainRAM_Buffer;
+
+__attribute__((section(".soundMainRAMFunc")))
+const int soundMainRAMFunc = SoundMainRAM_Buffer | 1;
+
+__attribute__((section(".soundMainRAMWordCnt")))
+const short soundMainRAMWordCnt = 0x200;
